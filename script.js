98c1893260ccb495e7651f98d2fcb0e6a8c2e4e3
YeyingG <karinaguo0708@gmail.com>
2026-06-23 22:18:05 -0400

    feedback, demo

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const year=document.querySelectorAll('[data-year]'); year.forEach(x=>x.textContent=new Date().getFullYear());
+const year = document.querySelectorAll('[data-year]');
+year.forEach(x => x.textContent = new Date().getFullYear());
+
+const readBetweenDemoLink = document.querySelector('#readBetweenDemoLink');
+if (readBetweenDemoLink) {
+  readBetweenDemoLink.addEventListener('click', event => {
+    event.preventDefault();
+    window.location.href = readBetweenDemoLink.href;
+  });
+}
